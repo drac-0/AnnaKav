0ceb8f26e9ef295eb61d374fd55b403bb96714a4
drac-0 <muhammadfaiz050607@gmail.com>
2026-09-23 04:42:22 +0800

    open mode change and the location of log bin

--- headerUndUtil/chprelated.c
+++ headerUndUtil/chprelated.c
@@ -1,9 +1,10 @@
+#include <fcntl.h>
 #include <time.h>
 #include "./sha.h"
 
 void Checkpoint(){
       time_t local = time(NULL);
-      int Fp = open("log.bin", O_WRONLY | O_CREAT);
+      int Fp = open("/home/draco/log.bin", O_WRONLY | O_CREAT | O_TRUNC);
       if (Fp == -1) {
             perror("An error encountered while trying to open the file from checkpoint func");
       }
